3f264b0c2bad579bc790daa67eee3d6c6f16ddb5
Yugharsh1 <130207729+Yugharsh1@users.noreply.github.com>
2023-08-07 18:47:56 +0530

    Add files via upload

---
+++ chat_room.js
@@ -0,0 +1,12 @@
+ var firebaseConfig = {
+    apiKey: "AIzaSyDTDpwgXcsj3owQNqHbpMztgEYuHSGp0sI",
+    authDomain: "kwitterx-159d9.firebaseapp.com",
+    databaseURL: "https://kwitterx-159d9-default-rtdb.firebaseio.com",
+    projectId: "kwitterx-159d9",
+    storageBucket: "kwitterx-159d9.appspot.com",
+    messagingSenderId: "67922458426",
+    appId: "1:67922458426:web:b641d4c214e3d30798c203"
+  };
+
+  firebase.initializeApp(firebaseConfig);
+  
